90633fab0d79556d127ee811a4d657ce7e0981e6
nearbyall <notzloyhuman@gmail.com>
2023-06-04 19:27:28 +0300

    feat: update before ci/cd

--- web/src/main/java/by/fin/web/RpaApplication.java
+++ web/src/main/java/by/fin/web/RpaApplication.java
@@ -3,12 +3,13 @@ package by.fin.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "by.fin.repository")
 @SpringBootApplication(scanBasePackages = "by.fin")
 @EntityScan("by.fin.repository.*")
-public class RpaApplication {
+public class RpaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RpaApplication.class, args);
